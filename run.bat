4f53f12d6804c91dd4f5d60d0de0b0bd0a78a1a5
Pari <pariagarwal3535@gmail.com>
2026-03-29 18:50:32 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,24 +1,6 @@
 @echo off
-echo ============================================
-echo  Memory Game - Build and Run
-echo ============================================
-
-:: Create output directory
-if not exist bin mkdir bin
-
-:: Compile all Java files
 echo Compiling...
-javac -d bin -sourcepath src src\Main.java
-
-if %errorlevel% neq 0 (
-    echo.
-    echo BUILD FAILED. Make sure JDK is installed and in PATH.
-    pause
-    exit /b 1
-)
-
-echo Compilation successful!
-echo.
+javac -d bin -cp "lib/*" -sourcepath src src/Main.java
 echo Launching Memory Game...
-java -cp bin Main
+java -cp "bin;lib/*" Main
 pause
